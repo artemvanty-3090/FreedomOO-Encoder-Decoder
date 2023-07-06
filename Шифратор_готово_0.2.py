5c2b4c4ca70713a630b4bd70f2c969c2c7a38455
bovantyman <79647023+artemvanty-3090@users.noreply.github.com>
2023-07-06 15:02:43 +0300

    Add files via upload

---
+++ Шифратор_готово_0.2.py
@@ -0,0 +1,17 @@
+import random
+
+def encrypt_message(message):
+    encrypted_message = ""
+    for letter in message:
+        if letter == " ":
+            encrypted_message += "2025" + ''.join(random.choice('abcdefghijklmnopqrstuvwxyz') for _ in range(3))
+        elif letter == ",":
+            encrypted_message += "3025" + ''.join(random.choice('abcdefghijklmnopqrstuvwxyz') for _ in range(3))
+        else:
+            ascii_code = ord(letter)
+            encrypted_message += "{:03d}".format(ascii_code) + ''.join(random.choice('abcdefghijklmnopqrstuvwxyz') for _ in range(3))
+    return encrypted_message
+
+user_input = input("Введите сообщение на русском языке: ")
+encrypted = encrypt_message(user_input)
+print("Зашифрованное сообщение:", encrypted)
